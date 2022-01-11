bc397478b22bfa485ec05d8aa84b7b4f025fc0e1
shadowbattalion <hafizspaceumar@gmail.com>
2022-01-11 13:11:34 +0000

    Done with animation

--- personal-website/src/pages/Home.js
+++ personal-website/src/pages/Home.js
@@ -1,7 +1,10 @@
 import React from 'react'
-import Button from 'react-bootstrap/Button'
+// import Button from 'react-bootstrap/Button'
 import Image from 'react-bootstrap/Image';
-import logo from '../logo.png'
+// import logo from '../logo.png'
+import ha from '../images/ha.png'
+import ain from '../images/ain.png'
+import mim from '../images/mim.png'
 
 
 export default function Home(){
@@ -11,16 +14,20 @@ export default function Home(){
 
         <React.Fragment>
             <div  id="home" className='background'>
-                
-                <div id="logo-box">
-                    <Image className='logo' src={logo} fluid />
-                </div>
+                <div id="components">
+                    <div id="logo-outer-box">
+                        <div id="logo-inner-box">
+                            <Image id='logo-ha' src={ha} fluid />
+                            <Image id='logo-mim' src={mim} fluid />
+                            <Image id='logo-ain' src={ain} fluid />
+                        </div>
+                    </div>
 
-                <div id="intro">
-                    <h1 id="title">HELLO! I AM HAFIZ</h1>
-                    <h2 id="sub-title">Welcome to my personal website!</h2>
-                </div>
-                
+                    <div id="intro">
+                        <h1 id="title">HELLO! I AM HAFIZ</h1>
+                        <h2 id="sub-title">Welcome to my personal website!</h2>
+                    </div>
+                </div>   
             </div>
 
         </React.Fragment>

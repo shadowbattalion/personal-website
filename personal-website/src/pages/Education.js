483457d9e9286c5294abe34fb4d6ac7d621d2fa0
shadowbattalion <hafizspaceumar@gmail.com>
2022-01-13 13:08:37 +0000

    added grid bootstrap to the cards

--- personal-website/src/pages/Education.js
+++ personal-website/src/pages/Education.js
@@ -9,48 +9,136 @@ export default function Education(){
 
         <React.Fragment>
             <div  id="education" className='background'>
-                <div className='shade components'>
-                    <h1 className='panel-title'>Education</h1>
-                    <Card className="card-properties" style={{ width: '18rem' }}>
-                        <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-                            <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                    <Card className="card-properties" style={{ width: '18rem' }}>
-                        <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-                            <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                    <Card className="card-properties" style={{ width: '18rem' }}>
-                        <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-                            <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                    <Card className="card-properties" style={{ width: '18rem' }}>
-                        <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-                            <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
+                <div className='shade'>
+                    <div class="container components">
+                        <h1 className='panel-title'>Education</h1>
+                        
+                        <div className="row row-cols-1 row-cols-md-2 row-cols-lg-5">
+                                
+                            <div className="card-container col" style={{ width: '20rem' }}>
+                                <div className="card-container-inner">
+                                    <div className="card-front">
+                                        <div className='card-back-inner'>
+                                            <h1 className='card-back-title'>HAFIZ</h1>
+                                        </div>   
+                                    </div>
+                                    <div className="card-back">     
+                                        <Card.Body>
+                                            <Card.Title>Card Title</Card.Title>
+                                            <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                                            <Card.Text>
+                                            Some quick example text to build on the card title and make up the bulk of
+                                            the card's content.
+                                            </Card.Text>
+                                        </Card.Body>   
+                                    </div>
+                                </div>
+                            </div>
+                                
+                            <div className="card-container col" style={{ width: '20rem' }}>
+                                <div className="card-container-inner">
+                                    <div className="card-front">
+                                        <div className='card-back-inner'>
+                                            <h1 className='card-back-title'>HAFIZ</h1>
+                                        </div>   
+                                    </div>
+                                    <div className="card-back">     
+                                        <Card.Body>
+                                            <Card.Title>Card Title</Card.Title>
+                                            <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                                            <Card.Text>
+                                            Some quick example text to build on the card title and make up the bulk of
+                                            the card's content.
+                                            </Card.Text>
+                                        </Card.Body>   
+                                    </div>
+                                </div>
+                            </div>
+                                
+                            <div className="card-container col" style={{ width: '20rem' }}>
+                                <div className="card-container-inner">
+                                    <div className="card-front">
+                                        <div className='card-back-inner'>
+                                            <h1 className='card-back-title'>HAFIZ</h1>
+                                        </div>   
+                                    </div>
+                                    <div className="card-back">     
+                                        <Card.Body>
+                                            <Card.Title>Card Title</Card.Title>
+                                            <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                                            <Card.Text>
+                                            Some quick example text to build on the card title and make up the bulk of
+                                            the card's content.
+                                            </Card.Text>
+                                        </Card.Body>   
+                                    </div>
+                                </div>
+                            </div>
+                                
+                            <div className="card-container col" style={{ width: '20rem' }}>
+                                <div className="card-container-inner">
+                                    <div className="card-front">
+                                        <div className='card-back-inner'>
+                                            <h1 className='card-back-title'>HAFIZ</h1>
+                                        </div>   
+                                    </div>
+                                    <div className="card-back">     
+                                        <Card.Body>
+                                            <Card.Title>Card Title</Card.Title>
+                                            <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                                            <Card.Text>
+                                            Some quick example text to build on the card title and make up the bulk of
+                                            the card's content.
+                                            </Card.Text>
+                                        </Card.Body>   
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="card-container col" style={{ width: '20rem' }}>
+                                <div className="card-container-inner">
+                                    <div className="card-front">
+                                        <div className='card-back-inner'>
+                                            <h1 className='card-back-title'>HAFIZ</h1>
+                                        </div>   
+                                    </div>
+                                    <div className="card-back">     
+                                        <Card.Body>
+                                            <Card.Title>Card Title</Card.Title>
+                                            <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                                            <Card.Text>
+                                            Some quick example text to build on the card title and make up the bulk of
+                                            the card's content.
+                                            </Card.Text>
+                                        </Card.Body>   
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            <div className="card-container col" style={{ width: '20rem' }}>
+                                <div className="card-container-inner">
+                                    <div className="card-front">
+                                        <div className='card-back-inner'>
+                                            <h1 className='card-back-title'>HAFIZ</h1>
+                                        </div>   
+                                    </div>
+                                    <div className="card-back">     
+                                        <Card.Body>
+                                            <Card.Title>Card Title</Card.Title>
+                                            <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                                            <Card.Text>
+                                            Some quick example text to build on the card title and make up the bulk of
+                                            the card's content.
+                                            </Card.Text>
+                                        </Card.Body>   
+                                    </div>
+                                </div>
+                            </div>
+           
+                        </div>
+
+                    </div>  
                 </div>
             </div>
         </React.Fragment>
--- personal-website/src/pages/Work.js
+++ personal-website/src/pages/Work.js
@@ -10,92 +10,95 @@ export default function Work(){
 
         <React.Fragment>
             <div  id="work-experiences" className='background'>
-                <div className='shade components'>
-                    <h1 className='panel-title'>Work Experiences</h1>
-
-                    <div className="card-container" style={{ width: '18rem' }}>
-                        <div className="card-container-inner">
-                            <div className="card-front">
-                                <div className='card-back-inner'>
-                                    <h1 className='card-back-title'>HAFIZ</h1>
-                                </div>   
-                            </div>
-                            <div className="card-back">     
-                                <Card.Body>
-                                    <Card.Title>Card Title</Card.Title>
-                                    <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-                                    <Card.Text>
-                                    Some quick example text to build on the card title and make up the bulk of
-                                    the card's content.
-                                    </Card.Text>
-                                </Card.Body>   
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="card-container" style={{ width: '18rem' }}>
-                        <div className="card-container-inner">
-                            <div className="card-front">
-                                <div className='card-back-inner'>
-                                    <h1 className='card-back-title'>HAFIZ</h1>
-                                </div>   
-                            </div>
-                            <div className="card-back">     
-                                <Card.Body>
-                                    <Card.Title>Card Title</Card.Title>
-                                    <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-                                    <Card.Text>
-                                    Some quick example text to build on the card title and make up the bulk of
-                                    the card's content.
-                                    </Card.Text>
-                                </Card.Body> 
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="card-container" style={{ width: '18rem' }}>
-                        <div className="card-container-inner">
-                            <div className="card-front">
-                                <div className='card-back-inner'>
-                                    <h1 className='card-back-title'>HAFIZ</h1>
-                                </div>   
-                            </div>
-                            <div className="card-back">     
-                                <Card.Body>
-                                    <Card.Title>Card Title</Card.Title>
-                                    <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-                                    <Card.Text>
-                                    Some quick example text to build on the card title and make up the bulk of
-                                    the card's content.
-                                    </Card.Text>
-                                </Card.Body> 
+                <div className='shade'>
+                    <div class="container components">
+                        <h1 className='panel-title'>Work Experiences</h1>
+                        
+                        <div className="row row-cols-1 row-cols-md-2 row-cols-lg-5">
                                 
+                            <div className="card-container col" style={{ width: '20rem' }}>
+                                <div className="card-container-inner">
+                                    <div className="card-front">
+                                        <div className='card-back-inner'>
+                                            <h1 className='card-back-title'>HAFIZ</h1>
+                                        </div>   
+                                    </div>
+                                    <div className="card-back">     
+                                        <Card.Body>
+                                            <Card.Title>Card Title</Card.Title>
+                                            <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                                            <Card.Text>
+                                            Some quick example text to build on the card title and make up the bulk of
+                                            the card's content.
+                                            </Card.Text>
+                                        </Card.Body>   
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-
-                    <div className="card-container" style={{ width: '18rem' }}>
-                        <div className="card-container-inner">
-                            <div className="card-front">
-                                <div className='card-back-inner'>
-                                    <h1 className='card-back-title'>HAFIZ</h1>
-                                </div>   
-                            </div>
-                            <div className="card-back">     
-                                <Card.Body>
-                                    <Card.Title>Card Title</Card.Title>
-                                    <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-                                    <Card.Text>
-                                    Some quick example text to build on the card title and make up the bulk of
-                                    the card's content.
-                                    </Card.Text>
-                                </Card.Body> 
                                 
+                            <div className="card-container col" style={{ width: '20rem' }}>
+                                <div className="card-container-inner">
+                                    <div className="card-front">
+                                        <div className='card-back-inner'>
+                                            <h1 className='card-back-title'>HAFIZ</h1>
+                                        </div>   
+                                    </div>
+                                    <div className="card-back">     
+                                        <Card.Body>
+                                            <Card.Title>Card Title</Card.Title>
+                                            <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                                            <Card.Text>
+                                            Some quick example text to build on the card title and make up the bulk of
+                                            the card's content.
+                                            </Card.Text>
+                                        </Card.Body>   
+                                    </div>
+                                </div>
                             </div>
+                                
+                            <div className="card-container col" style={{ width: '20rem' }}>
+                                <div className="card-container-inner">
+                                    <div className="card-front">
+                                        <div className='card-back-inner'>
+                                            <h1 className='card-back-title'>HAFIZ</h1>
+                                        </div>   
+                                    </div>
+                                    <div className="card-back">     
+                                        <Card.Body>
+                                            <Card.Title>Card Title</Card.Title>
+                                            <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                                            <Card.Text>
+                                            Some quick example text to build on the card title and make up the bulk of
+                                            the card's content.
+                                            </Card.Text>
+                                        </Card.Body>   
+                                    </div>
+                                </div>
+                            </div>
+                                
+                            <div className="card-container col" style={{ width: '20rem' }}>
+                                <div className="card-container-inner">
+                                    <div className="card-front">
+                                        <div className='card-back-inner'>
+                                            <h1 className='card-back-title'>HAFIZ</h1>
+                                        </div>   
+                                    </div>
+                                    <div className="card-back">     
+                                        <Card.Body>
+                                            <Card.Title>Card Title</Card.Title>
+                                            <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                                            <Card.Text>
+                                            Some quick example text to build on the card title and make up the bulk of
+                                            the card's content.
+                                            </Card.Text>
+                                        </Card.Body>   
+                                    </div>
+                                </div>
+                            </div>
+           
                         </div>
-                    </div>
-                   
-                    
+
+                    </div>  
                 </div>
             </div>
 

c67e8cb020fe94001172a073ab605ab18282497a
shadowbattalion <hafizspaceumar@gmail.com>
2022-01-17 07:05:50 +0000

    Finalise projects and contacts section

--- personal-website/src/pages/Contact.js
+++ personal-website/src/pages/Contact.js
@@ -12,7 +12,7 @@ export default function Contact(){
                 <div style={{paddingTop:"100px"}}>
                     <div class="container components">
                         <h1 className='contact-title'>Contact</h1>
-                        <h2 className='contact-subtitle'>hafizspaceumar@gmail.com</h2>
+                        <h2 className='contact-subtitle'><a href="mailto:hafizspaceumar@gmail.com">hafizspaceumar@gmail.com</a></h2>
                         <h2 className='contact-subtitle'><a href="https://linkedin.com/in/hafiz-umar-81219b217" target="_blank" rel="noopener noreferrer">My LinkedIn</a></h2>
                     </div>  
                 </div>

8abe2fd36b55fe8acedb75f445dbfe12306c4c40
shadowbattalion <hafizspaceumar@gmail.com>
2022-01-17 11:40:16 +0000

    done with about page

--- personal-website/src/pages/About.js
+++ personal-website/src/pages/About.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import Card from "react-bootstrap/Card"
 
 export default function Projects(){
 
@@ -12,7 +12,20 @@ export default function Projects(){
                 <div className='shade'>
                     <div class="container components">
                         <h1 className='panel-title'>About Me</h1>
-                        <p className='about-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean imperdiet turpis ac porta pharetra. Fusce molestie ligula eget mi ullamcorper pharetra. Quisque iaculis lobortis bibendum. Morbi nec dapibus lorem. Pellentesque id neque laoreet, ultrices augue et, tempor nunc. Integer eu ex felis. Sed aliquam urna eget sapien sodales dignissim. Pellentesque tellus enim, faucibus non urna sed, pharetra pulvinar arcu. Sed at mattis nulla. Nunc vel egestas enim, nec finibus velit. Etiam dignissim tortor odio, feugiat tincidunt nunc scelerisque vel. Quisque eu nisl quis lorem scelerisque consectetur. Nunc sed dolor rhoncus, sagittis mauris at, euismod tortor. </p>
+                        <Card>
+                            <Card.Body style={{color:"black", backgroundColor:"#fde8eb"}}>
+                                <p className='about-text' style={{fontWeight:"bold"}}>Introduction</p>
+                                <div className="card-divider"></div>
+                                <p className='about-text'>Hi! I am Hafiz. I love doing frontend development. I started getting into IT about 8 years ago when I enrolled in Singapore Polytechnic in IT Security course. After that I continued studying in the same field in Singapore Institute of Technology. After graduation, I found a job as a website pentester.</p>
+                                <p className='about-text' style={{fontWeight:"bold"}}>A Change of Heart</p>
+                                <div className="card-divider"></div>
+                                <p className='about-text'>After some time being a pentester, I realise that I love building things and not breaking them down. So I tried doing Python as a job, since I am already used to using Python. I found a job as a backend developer. However, I realise that I have much more to learn about website development. Soon after, I enrolled in a web development course in Trent Global, which offers a diploma.</p>
+                                <p className='about-text' style={{fontWeight:"bold"}}>A New Pursuit and A New Ambition</p>
+                                <div className="card-divider"></div>
+                                <p className='about-text'>While in Trent Global, I was exposed to fullstack development. However, I realised that I love doing frontend and I like doing design and meeting user experience needs. Being detail-oriented in nature, I would spent many hours to perfect the design and visuals of the website. Being a creative person, I like to experiment with things so that the website will look impressive. I am looking forward to new projects with you!</p>
+                            </Card.Body>
+                        </Card>
+                        
                        
 
                     </div>  
